9a9247eaa35e2e1caa5ab744a6ff3711a37cf742
Ragil14 <rahil.gilani@gmail.com>
2022-01-25 16:45:31 -0600

    added route from client to server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const { animals } = require('./data/animals');
 
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+});
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
