5829803fee918d81579fd28546eb1f3d9ee7b3f5
Ragil14 <rahil.gilani@gmail.com>
2022-01-26 15:16:03 -0600

    added filterByQuery() function to create query

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const { animals } = require('./data/animals');
 
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     // change .json to .send here for small data
