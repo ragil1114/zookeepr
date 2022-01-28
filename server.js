950bbf6592f03848d93da4942fe68da4fe992ceb
Ragil14 <rahil.gilani@gmail.com>
2022-01-28 17:53:25 -0600

    deleted console.log

--- server.js
+++ server.js
@@ -82,7 +82,6 @@ app.get('/api/animals/:id', (req, res) => {
 
 app.post('/api/animals', (req, res) => {
   // req.body is where our incoming content will be
-  console.log(req.body);
   res.json(req.body);
 });
 
