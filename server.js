76e29eadc175628f08ff71e559e11836b185ee2d
Ragil14 <rahil.gilani@gmail.com>
2022-01-28 17:23:16 -0600

    parse incoming data

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
