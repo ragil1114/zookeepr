32f1daede2fbd7f906f4d796e26defa7bf94f7cd
Ragil14 <rahil.gilani@gmail.com>
2022-01-28 18:56:31 -0600

    added fs.writeFileSync() Method to createNewAnimal() Function

--- server.js
+++ server.js
@@ -58,6 +58,10 @@ function findById(id, animalsArray) {
 function createNewAnimal(body, animalsArray) {
   const animal = body;
   animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
 
   return animal;
 }
