1897fe54dbed0a573a2775cc6bee3128de35c70e
Ragil14 <rahil.gilani@gmail.com>
2022-01-28 19:13:04 -0600

    added validateAnimal() function

--- server.js
+++ server.js
@@ -65,6 +65,22 @@ function createNewAnimal(body, animalsArray) {
   return animal;
 }
 
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
