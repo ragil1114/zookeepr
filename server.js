05766749312a67754207b925ed9ebdeaec7f1efa
Ragil14 <rahil.gilani@gmail.com>
2022-02-03 00:01:28 -0600

    deleted Routes from server.js & added them to animalRoutes.js file

--- server.js
+++ server.js
@@ -12,60 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-app.get('/api/animals', (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  // change .json to .send here for small data
-  res.json(results);
-});
-
-app.get('/api/animals/:id', (req, res) => {
-  const result = findById(req.params.id, animals);
-  if (result) {
-    res.json(result);
-  } 
-  else {
-    res.send(404);
-  }
-});
-
-app.post('/api/animals', (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = animals.length.toString();
-  // if any data in req.body is incorrect, send 400 error back
-  if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted.');
-  } 
-  else {
-    // add animal to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animals);
-    // req.body is where our incoming content will be
-    res.json(animal);
-  }
-});
-
-// Route used to create a homepage for a server. Server responds with index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-// Route to display animals.html 
-app.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-// Route to display zookeepers.html
-app.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
-// Wildcard Route to display index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
