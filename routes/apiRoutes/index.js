c41076f52567eb97ab18eb8d2bafbb29a24fe2d5
Ragil14 <rahil.gilani@gmail.com>
2022-02-03 01:32:35 -0600

    updated code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
 
+// Middleware that instructs the server to make certain files readily available.
 router.use(animalRoutes);
 router.use(require('./zookeeperRoutes'));
 
