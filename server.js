3aacd80beae3c1013727ef4621a2f47cbf02ab60
Ragil14 <rahil.gilani@gmail.com>
2022-02-03 02:16:35 -0600

    clean code

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animals');
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
