0424a296cd8d8afc3f681b0be2f02a8a101cfc1b
Ragil14 <rahil.gilani@gmail.com>
2022-02-03 00:56:11 -0600

    cleaned code in animals.test.js file

--- __tests__/animals.test.js
+++ __tests__/animals.test.js
@@ -1,10 +1,5 @@
 const fs = require("fs");
-const {
-  filterByQuery,
-  findById,
-  createNewAnimal,
-  validateAnimal,
-} = require("../lib/animals.js");
+const { filterByQuery, findById, createNewAnimal, validateAnimal } = require("../lib/animals.js");
 const { animals } = require("../data/animals");
 
 jest.mock('fs');
